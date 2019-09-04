9798c6dc10fc3e1a6bb9471d5d5999bae1c23646
jameela <jhamody@campus.haifa.ac.il>
2019-09-04 14:10:23 +0300

    jj

--- app/src/main/java/org/tsofen/ourstory/StoryTeam/ViewStory.java
+++ app/src/main/java/org/tsofen/ourstory/StoryTeam/ViewStory.java
@@ -20,10 +20,6 @@ public class ViewStory extends AppCompatActivity implements Serializable {
 
     ImageButton ib;
     ImageButton share;
-<<<<<<< HEAD
-=======
-
->>>>>>> 3b34a92f3c0f6f2253abf7375365704b5d89ad60
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,39 +77,32 @@ public class ViewStory extends AppCompatActivity implements Serializable {
         ImageView image3 = findViewById(R.id.imageView6);
         image3.setImageResource(story.getTag_icon3());
 
-<<<<<<< HEAD
-         share=findViewById (R.id.ShareTxButton);
-         share.setOnClickListener(new View.OnClickListener(){
-             @Override
-          public void onClick(View v){
 
-              share_it (v);
-         }
-    });
+         share=findViewById (R.id.ShareTxButton);
+
 }
-=======
-    }
->>>>>>> 3b34a92f3c0f6f2253abf7375365704b5d89ad60
+
+
+
 
     public void launchSearchActivity(View view) {
         Intent i = new Intent(ViewStory.this, SearchStory.class);
         startActivity(i);
     }
 
-<<<<<<< HEAD
-    public void share_it(View view) {
 
-        Intent myIntent = new Intent(Intent.ACTION_SEND);
-        myIntent.setType ("text/plain");
-        String shareBody="the story of: pini cohen";
-        String sharesub="the subject is:";
-        myIntent.putExtra (Intent.EXTRA_SUBJECT,shareBody);
-        myIntent.putExtra (Intent.EXTRA_TEXT,sharesub);
-        startActivity (Intent.createChooser (myIntent,"share using"));
+//    public void share_it(View view) {
+//
+//        Intent myIntent = new Intent(Intent.ACTION_SEND);
+//        myIntent.setType ("text/plain");
+//        String shareBody="the story of: pini cohen";
+//        String sharesub="the subject is:";
+//        myIntent.putExtra (Intent.EXTRA_SUBJECT,shareBody);
+//        myIntent.putExtra (Intent.EXTRA_TEXT,sharesub);
+//        startActivity (Intent.createChooser (myIntent,"share using"));
+//
+//    }
 
-    }
-
-=======
     public void launchShare(View view) {
 //        share = findViewById(R.id.sharebtn);
 
@@ -125,5 +114,5 @@ public class ViewStory extends AppCompatActivity implements Serializable {
 //        myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
 //        startActivity(Intent.createChooser(myIntent, "Share using: "));
     }
->>>>>>> 3b34a92f3c0f6f2253abf7375365704b5d89ad60
+
 }
